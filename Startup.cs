971d3c4c733254b2f45948419ea260b9cd0b3741
John Dobrota <adobrota@ucsc.edu>
2020-01-29 18:42:00 -0800

    Initial Commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeeChat.Startup))]
+namespace WeeChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
